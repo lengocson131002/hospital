d91cb10327e6b638e482a0f2154226b43827b8fb
lengocson <leson131002@gmail.com>
2023-06-01 16:29:58 +0700

    init commit

---
+++ data.sql
@@ -0,0 +1,113 @@
+use HospitalBooking
+go
+
+create table dbo.Bill
+(
+    Id                 int identity
+        constraint Bill_pk
+        primary key,
+    Price              bigint      not null,
+    Status             varchar(50) not null,
+    CreatedAt          datetime,
+    UpdatedAt          datetime,
+    CheckoutAt         datetime,
+    PatientName        nvarchar(255),
+    PatientEmail       nvarchar(255),
+    PatientPhoneNumber nvarchar(255)
+)
+    go
+
+create table dbo.Department
+(
+    Id          int identity
+        constraint Department_pk
+        primary key,
+    Name        nvarchar(255)  not null,
+    Description nvarchar(1000) not null
+)
+    go
+
+create table dbo.Account
+(
+    Id           int identity
+        constraint Account_pk
+        primary key,
+    Avatar       nvarchar(255),
+    FirstName    nvarchar(255),
+    LastName     nvarchar(255),
+    DepartmentId int
+        constraint Account_Department_Id_fk
+            references dbo.Department,
+    Gender       varchar(50),
+    DOB          date,
+    PhoneNumber  varchar(255),
+    Email        varchar(255) not null,
+    Description  nvarchar(1000),
+    CreatedAt    datetime,
+    UpdatedAt    datetime,
+    Role         varchar(50)  not null,
+    Token        varchar(255),
+    Password     varchar(255),
+    Address      nvarchar(500)
+)
+    go
+
+create table dbo.Shift
+(
+    Id        int identity
+        constraint Shift_pk
+        primary key,
+    DoctorId  int  not null
+        constraint Shift_User_Id_fk
+            references dbo.Account,
+    Room      int  not null,
+    Slot      int  not null,
+    CreatedAt datetime,
+    UpdatedAt datetime,
+    Date      date not null
+)
+    go
+
+create table dbo.Appointment
+(
+    Id           int identity,
+    BookerId     int         not null
+        constraint Booking_User_Id_booker
+            references dbo.Account,
+    DoctorId     int         not null
+        constraint Booking_User_Id_doctor
+            references dbo.Account,
+    ShiftId      int         not null
+        constraint Appointment_Shift_Id_fk
+            references dbo.Shift,
+    PatientName  nvarchar(255),
+    PatientPhone nvarchar(255),
+    Status       varchar(50) not null,
+    PatientEmail nvarchar(255),
+    PatientNote  nvarchar(1000),
+    DoctorNote   nvarchar(1000),
+    CreatedAt    datetime,
+    UpdatedAt    datetime,
+    CanceledAt   datetime,
+    FinishedAt   datetime,
+    BillId       int
+        constraint Appointment_Bill_Id_fk
+            references dbo.Bill
+)
+    go
+
+create table dbo.Token
+(
+    Id        int identity
+        constraint Token_pk
+        primary key,
+    Token     varchar(255) not null,
+    CreatedAt datetime     not null,
+    AccountId int          not null
+        constraint Token_Account_Id_fk
+            references dbo.Account,
+    ExpiredAt datetime,
+    Type      nvarchar(50) not null
+)
+    go
+
